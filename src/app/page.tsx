037c74b2dc7d0aca5116fae3c746f53aba0b61d1
sogo0 <sogo.rei812@mail.kyutech.jp>
2025-02-10 17:37:11 +0900

    create header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import styles from './styles/styles.module.css'
+
+//components
 import Title from "./components/title/title";
+import Header from "./components/header/header";
 
 export default function Home() {
 
     return (
     <>
+    <Header/>
     <Title/>
     
     </>
