7b5525876e7af026893acefd2ca73ca2a45c187d
sogo0 <sogo.rei812@mail.kyutech.jp>
2025-02-14 17:21:28 +0900

    adjust produvt2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import styles from './styles/styles.module.css'
 import Title from "./components/title/title";
 import Header from "./components/header/header";
 import Product1 from "./components/Products/product1";
+import Product2 from "./components/Products/product2";
 
 export default function Home() {
 
@@ -12,8 +13,10 @@ export default function Home() {
     <>
     <Header/>
     <Title/>
-    <div className="flex"></div>
-    <Product1/>
+    <div className="flex flex-col">
+      <Product1/>
+      <Product2/>
+    </div>
     
     </>
         
