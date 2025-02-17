368b40e1f33d52e87c6eea973342f2dbfff033a4
sogo0 <sogo.rei812@mail.kyutech.jp>
2025-02-17 17:44:42 +0900

    pinned index

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -15,27 +15,33 @@ export default function Fotter() {
                         <div className={fotterstyles.footermain2}>Thank you for your time and interest!</div>
                     </div>
                 </div>
+                <div className={fotterstyles.footerrectangle}>
                     <div className={fotterstyles.footerbox}>
-                        <div className={fotterstyles.footertext}>
-                            
-                            <div className={fotterstyles.footermain2}>sogo Rei MyPortfolio</div>
-                                <div className={fotterstyles.fottericons}>
-                                    <Link href="https://www.instagram.com/0_s0g0" >
-                                        <FontAwesomeIcon icon={faInstagram} className={fotterstyles.icon}/>
-                                    </Link>
-                                    <Link href="https://x.com/0_s0g0" >
-                                        <FontAwesomeIcon icon={faXTwitter} className={fotterstyles.icon}/>
-                                    </Link>
-                                    <Link href="https://www.facebook.com/share/1WFxQ2T4mg/" >
-                                        <FontAwesomeIcon icon={faFacebook} className={fotterstyles.icon}/>
-                                    </Link>
-                                    <Link href="https://github.com/0-s0g0" >
-                                        <FontAwesomeIcon icon={faGithub} className={fotterstyles.icon}/>
-                                    </Link>
+                        <div className={fotterstyles.footerSubtext}>
+                            <div className={fotterstyles.footertext2}>MyPortfolio</div>
+                            <div className={fotterstyles.footertext3}>-SogoRei Cording Products-</div>
+                        </div>
+
+                                <div className={fotterstyles.footermain2}>
+                                    <div className={fotterstyles.fottericons}>
+                                        <Link href="https://www.instagram.com/0_s0g0" >
+                                            <FontAwesomeIcon icon={faInstagram} className={fotterstyles.icon}/>
+                                        </Link>
+                                        <Link href="https://x.com/0_s0g0" >
+                                            <FontAwesomeIcon icon={faXTwitter} className={fotterstyles.icon}/>
+                                        </Link>
+                                        <Link href="https://www.facebook.com/share/1WFxQ2T4mg/" >
+                                            <FontAwesomeIcon icon={faFacebook} className={fotterstyles.icon}/>
+                                        </Link>
+                                        <Link href="https://github.com/0-s0g0" >
+                                            <FontAwesomeIcon icon={faGithub} className={fotterstyles.icon}/>
+                                        </Link>
+                                    </div>
                                 </div>
                                 
-                        </div>
+                        
                     </div>
+                </div>
                 
             </div>
         </div>
