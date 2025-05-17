64f671852f62ad0c3324c1282117e05e2db5e48b
root <root@LAPTOP-D4FDAP63.>
2025-05-17 15:03:54 +0900

    charck git

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "有限会社北九電機",
-  description: "配電盤の製造、修理及び工事施工、電気設備工事など幅広いサービスを提供しています。",
+  description: "配電盤の製造、修理及び工事施工、電気設備工事など幅広いサービスを提供しています",
 }
 
 export default function RootLayout({
