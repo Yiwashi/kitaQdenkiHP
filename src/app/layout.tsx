e38f67b818993f7e92ca29c02bb16898bac4aca0
root <root@LAPTOP-D4FDAP63.>
2025-05-17 15:17:32 +0900

    ini

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "有限会社北九電機",
-  description: "配電盤の製造、修理及び工事施工、電気設備工事など幅広いサービスを提供しています",
+  description: "各種製造機器の制御盤等の設計・製造、電気・通信工事、建築物・工作物石綿含有調査など幅広いサービスを提供しています",
 }
 
 export default function RootLayout({
